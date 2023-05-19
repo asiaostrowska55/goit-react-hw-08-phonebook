388b79739edbad311652d503a4a2342587549b6e
asiaostrowska55 <110023821+asiaostrowska55@users.noreply.github.com>
2023-05-19 20:51:40 +0200

    post contacts issue

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -13,22 +13,22 @@ const ContactForm = () => {
     const form = event.target;
 
     let isContact;
-
     contacts.forEach(person => {
       if (form.name.value.toLowerCase() === person.name.toLowerCase()) {
         isContact = true;
       }
     });
-    isContact
-      ? alert(`${form.name.value} is already in contacts!`)
-      : dispatch(
-          addContact({
-            id: nanoid(),
-            name: form.name.value,
-            phone: form.phone.value,
-          })
-        );
-
+    if (isContact) {
+      alert(`${form.name.value} is already in contacts!`);
+    }
+    if (!isContact) {
+      dispatch(
+        addContact({
+          name: form.name.value,
+          number: form.number.value,
+        })
+      );
+    }
     form.reset();
   };
 
@@ -42,10 +42,10 @@ const ContactForm = () => {
           Name
         </label>
         <input
-          className={css.input}
           id={nameId}
           type="text"
           name="name"
+          placeholder="Enter name"
           pattern="^[a-zA-Zа-яА-Я\u0104\u0105\u0106\u0107\u0118\u0119\u0141\u0142\u0143\u0144\u00D3\u00F3\u015A\u015B\u0179\u017A\u017B\u017C]+(([' \-][a-zA-Zа-яА-Я \u0104\u0105\u0106\u0107\u0118\u0119\u0141\u0142\u0143\u0144\u00D3\u00F3\u015A\u015B\u0179\u017A\u017B\u017C])?[a-zA-Zа-яА-Я \u0104\u0105\u0106\u0107\u0118\u0119\u0141\u0142\u0143\u0144\u00D3\u00F3\u015A\u015B\u0179\u017A\u017B\u017C]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
@@ -54,14 +54,13 @@ const ContactForm = () => {
           Number
         </label>
         <input
-          className={css.input}
           id={numberId}
           type="tel"
-          name="phone"
+          name="number"
+          placeholder="Enter number"
           pattern="\+?\d{1,4}?[\-.\s]?\(?\d{1,3}?\)?[\-.\s]?\d{1,4}[\-.\s]?\d{1,4}[\-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
-          autoComplete="tel"
         />
         <button className={css.btn} type="submit">
           Add contact
--- src/redux/phonebook/contactSlicer.js
+++ src/redux/phonebook/contactSlicer.js
@@ -1,31 +1,40 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-export const fetchContacts = createAsyncThunk('contacts/fetchAll', async () => {
-  const response = await axios.get('/contacts');
-  return response.data;
-});
+axios.defaults.baseURL = 'https://connections-api.herokuapp.com/';
+
+export const fetchContacts = createAsyncThunk(
+  'contacts/fetchAll',
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios.get('/contacts');
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
 
 export const addContact = createAsyncThunk(
   'contacts/addContact',
-  async (contact, thunkAPI) => {
+  async ({ name, number }, thunkAPI) => {
     try {
-      const response = await axios.post('/contacts', contact);
+      const response = await axios.post('/contacts', { name, number });
       return response.data;
-    } catch (error) {
-      return thunkAPI.rejectWithValue(error.message);
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
     }
   }
 );
 
 export const deleteContact = createAsyncThunk(
-  'contact/deleteContact',
+  'contacts/deleteContact',
   async (contactId, thunkAPI) => {
     try {
       const response = await axios.delete(`/contacts/${contactId}`);
       return response.data;
-    } catch (error) {
-      return thunkAPI.rejectWithValue(error.message);
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
     }
   }
 );
